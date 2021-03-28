59768d4a9bc5490d69fcb0d10eec3b69afb84c34
Paul Richardson <developer@photographerafoot.com>
2021-03-29 09:10:04 +1100

    CHG: Load selected image

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -13,20 +13,11 @@ class DetailViewController: UIViewController {
 	var selectedImage: String?
 	
 	override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		if let imageToLoad = selectedImage {
+			imageView.image = UIImage(named: imageToLoad)
+		}
+	}
 
 }
