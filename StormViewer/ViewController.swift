e18a8b727107ad06d7fae09d878d379c1cc80ef0
Paul Richardson <developer@photographerafoot.com>
2021-04-23 08:53:47 +1000

    CHG: Load images on background thread
    Refactor image loading to a new method - loadImages()
    call loadImages on the background thread
    call reloadData() on the main thread

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UITableViewController {
 		title = "Storm Viewer"
 		navigationController?.navigationBar.prefersLargeTitles = true
 
+		performSelector(inBackground: #selector(loadImages), with: nil)
+
+	}
+
+	@objc fileprivate func loadImages() {
 		let fm = FileManager.default
 		let path = Bundle.main.resourcePath!
 		let items = try! fm.contentsOfDirectory(atPath: path)
@@ -27,8 +32,10 @@ class ViewController: UITableViewController {
 			}
 		}
 		pictures = pictures.sorted()
-		print(pictures)
-
+		
+		DispatchQueue.main.async { [weak self] in
+			self?.tableView.reloadData()
+		}
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
