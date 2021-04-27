2e125112c0a8a43a90accffe27094c352a7f555b
Paul Richardson <developer@photographerafoot.com>
2021-04-28 09:56:07 +1000

    CHG: add custom collection view cell

---
+++ StormViewer/PictureCell.swift
@@ -0,0 +1,13 @@
+//
+//  PictureCell.swift
+//  StormViewer
+//
+//  Created by Paul Richardson on 28/04/2021.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+	@IBOutlet var title: UILabel!
+	@IBOutlet var detail: UILabel!
+}
